b49ec588de3f16f8c4a7594cbafcdf62704b1011
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-08-16 18:18:18 +0200

    added bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
-<script setup>
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  name: 'travel-app',
+  components: { AppHeader }
+}
 </script>
 
 <template>
+  <!-- Header -->
+  <AppHeader />
+
 
 </template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: 'AppHeader',
+}
+</script>
+
+<template>
+    <header>
+        <h1 class="text-center">Travel App</h1>
+    </header>
+</template>
