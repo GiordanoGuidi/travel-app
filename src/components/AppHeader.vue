9f8c70973297cfdd41aa5fb616a40e6b4b6a1c1f
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-08-20 18:19:28 +0200

    added routerLink into AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,5 +7,11 @@ export default {
 <template>
     <header>
         <h1 class="text-center">Travel App</h1>
+        <ul class="d-flex list-unstyled justify-content-center gap-5">
+            <RouterLink :to="{ name: 'create-trip' }">
+                <li>Crea il tuo viaggio</li>
+            </RouterLink>
+            <li>I tuoi viaggi</li>
+        </ul>
     </header>
 </template>
