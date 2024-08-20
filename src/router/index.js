681baa3cca8c17dcc41111efe429635928e5dc81
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-08-20 18:16:43 +0200

    added route create-trip

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 //Importo le pagine
 import TripDetails from '../pages/TripDetails.vue';
+import CreateTrip from '../pages/CreateTrip.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -9,6 +10,9 @@ const router = createRouter({
     linkActiveClass: 'partial-active',
     linkExactActiveClass: 'active',
     routes: [
+        //Rotta per la creazione del viaggio
+        { path: '/create-trip', name: 'create-trip', component: CreateTrip },
+        //Rotta per il dettaglio del viaggio
         { path: '/trip/:id', name: 'trip-details', component: TripDetails },
 
     ]
