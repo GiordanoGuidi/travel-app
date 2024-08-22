847729202e98ad18a5f66c36fe69a1054d46c224
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-08-22 15:54:59 +0200

    created AddStopForm

---
+++ src/components/forms/AddStopForm.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'AddStopForm',
+    props: { stop: Object },
+    emit: ['remove-stop', 'submit-form'],
+}
+</script>
+
+<template>
+    <form @submit.prevent="submitForm">
+        <input v-model="stop.title" placeholder="Titolo della tappa">
+        <input v-model="stop.description" placeholder="Descrizione della tappa">
+        <!--Bottone per rimuovere la tappa-->
+        <button @click="$emit('remove-stop')">Rimuovi tappa</button>
+        <button @click="$emit('submit-form', stop)">Aggiungi</button>
+    </form>
+</template>
