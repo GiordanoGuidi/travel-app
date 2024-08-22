23f79f3924180b52461ebc3fa3237efe2a05b882
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-08-22 15:55:32 +0200

    added CreateTripForm component

---
+++ src/components/forms/CreateTripForm.vue
@@ -0,0 +1,26 @@
+<script>
+import { store } from '../../data/store';
+
+export default {
+    name: 'AppForm',
+    data: () => ({
+        trip: store.trip,
+    }),
+    methods: {
+        submitForm() {
+            // Emetto un custom event all'invio del form
+            this.$emit('submit-trip', this.trip);
+        }
+    }
+}
+</script>
+
+<template>
+    <!-- Form per creare un nuovo viaggio -->
+    <form @submit.prevent="submitForm">
+        <input type="text" v-model="trip.destination" placeholder="Meta del viaggio" required>
+        <input type="number" v-model="trip.duration" placeholder="Durata in giorni" required>
+        <input type="date" v-model="trip.start_date" placeholder="Data di inizio" required>
+        <button type="submit">Crea Viaggio</button>
+    </form>
+</template>
