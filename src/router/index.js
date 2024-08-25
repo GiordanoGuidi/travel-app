10e0cc048f464e5e146cd17dbea71c648f8ef047
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-08-25 15:55:28 +0200

    added route to the homepage

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 //Importo le pagine
 import TripDetails from '../pages/TripDetails.vue';
 import CreateTrip from '../pages/CreateTrip.vue';
+import HomePage from '../pages/HomePage.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -10,11 +11,12 @@ const router = createRouter({
     linkActiveClass: 'partial-active',
     linkExactActiveClass: 'active',
     routes: [
+        //Rotta per l'Homepage
+        { path: '/', name: 'home-page', component: HomePage },
         //Rotta per la creazione del viaggio
         { path: '/create-trip', name: 'create-trip', component: CreateTrip },
         //Rotta per il dettaglio del viaggio
         { path: '/trip/:id', name: 'trip-details', component: TripDetails },
-
     ]
 });
 export { router };
