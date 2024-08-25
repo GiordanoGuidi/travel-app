16d983d95e702d6a67742eb834c4e39fbcd3dfd5
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-08-25 15:55:55 +0200

    added Homepage page

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'HomePage',
+}
+</script>
+
+<template>
+    <div class="image-container">
+    </div>
+</template>
+<style>
+.image-container {
+    width: 100%;
+    height: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-image: url(https://www.donnamoderna.com/content/uploads/2020/04/Viaggiare-organizzando-un-viaggio-830x625.jpg);
+}
+</style>
