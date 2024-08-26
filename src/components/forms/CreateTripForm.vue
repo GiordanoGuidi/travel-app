d78d47084bab8703b087e89f5a7012d1e1819109
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-08-26 16:47:51 +0200

    added validation and style

--- src/components/forms/CreateTripForm.vue
+++ src/components/forms/CreateTripForm.vue
@@ -1,5 +1,6 @@
 <script>
 import { store } from '../../data/store';
+const regex = /^[a-zA-Z\s]+$/;
 
 export default {
     name: 'AppForm',
@@ -8,6 +9,7 @@ export default {
         errors: {},
     }),
     methods: {
+        //Metodo eseguito al submit del form
         submitForm() {
             //Invoco funzione per validazione del form
             if (this.validateForm(this.trip)) {
@@ -24,18 +26,20 @@ export default {
                 this.errors.destination = 'La destinazione è obbligatoria'
             } else if (!isNaN(destination)) {
                 this.errors.destination = 'La destinazione non può essere un numero'
+            } else if (!regex.test(destination)) {
+                this.errors.destination = 'La destinazione non può contenere numeri e caratteri speciali'
             }
             if (!start_date) {
                 this.errors.start_date = 'La data di partenza è obbligatoria'
             } else if (start_date < this.getCurrentDate()) {
                 this.errors.start_date = 'La data di partenza è passata'
             } else if (start_date > end_date) {
-                this.errors.start_date = 'La data di partenza non è valida'
+                this.errors.start_date = 'La data di partenza deve essere precedente alla data di rientro'
             }
             if (!end_date) {
                 this.errors.end_date = 'La data di rientro è obbligatoria'
             } else if (end_date < start_date) {
-                this.errors.end_date = 'La data di rientro non è valida'
+                this.errors.end_date = 'La data di rientro deve essere successiva alla data di partenza'
             }
             console.log(this.errors.destination, this.errors.start_date, this.errors.end_date)
             return Object.keys(this.errors).length === 0;
@@ -53,13 +57,14 @@ export default {
 </script>
 
 <template>
-    <div class="form-container d-flex align-items-center justify-content-center">
+    <div
+        :class="['form-container d-flex align-items-center justify-content-center', { 'has-error': Object.keys(errors).length > 0 }]">
         <!-- Form per creare un nuovo viaggio -->
         <form @submit.prevent="submitForm">
-            <div class="d-flex gap-2 flex-wrap">
-                <div class="d-flex gap-2 mb-2">
+            <div class="d-flex flex-wrap">
+                <div class="d-flex mb-2 col-4">
                     <!-- Destinazione -->
-                    <label for="destination" class="form-label">Destinazione: </label>
+                    <label for="destination" class="form-label mb-0 pe-2">Destinazione: </label>
                     <div>
                         <input class="rounded p-1 form-control" :class="{ 'is-invalid': this.errors.destination }"
                             id="destination" type="text" v-model="trip.destination" placeholder="Meta del viaggio">
@@ -67,8 +72,8 @@ export default {
                     </div>
                 </div>
                 <!-- Data di inizio -->
-                <div class="d-flex gap-2 mb-2 ">
-                    <label for="start" class="form-label">Partenza: </label>
+                <div class="d-flex gap-2 mb-2 col-4">
+                    <label for="start" class="form-label mb-0 ps-3">Partenza: </label>
                     <div>
                         <input class="rounded p-1 form-control pe-5" :class="{ 'is-invalid': this.errors.start_date }"
                             id="start" type="date" v-model="trip.start_date">
@@ -76,8 +81,8 @@ export default {
                     </div>
                 </div>
                 <!-- Data di fine -->
-                <div class="d-flex gap-2 mb-2">
-                    <label for="end" class="form-label">Rientro: </label>
+                <div class="d-flex gap-2 mb-2 col-4">
+                    <label for="end" class="form-label mb-0 ps-3">Rientro: </label>
                     <div>
                         <input class="rounded form-control p-1 pe-5" :class="{ 'is-invalid': this.errors.end_date }"
                             id="end" type="date" v-model="trip.end_date">
@@ -99,6 +104,11 @@ export default {
     width: 1000px;
     background-color: rgba(200, 200, 200, 0.5);
     border-radius: 10px;
+
+    // Aumento le dimensioni del form-container in caso di errori
+    &.has-error {
+        height: 300px;
+    }
 
     form {
         height: 40px;
