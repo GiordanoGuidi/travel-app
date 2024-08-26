0eba71056ecaae4931064f5fd4e2a058ffd19372
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-08-26 12:12:27 +0200

    removed style

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,16 +5,6 @@ export default {
 </script>
 
 <template>
-    <div class="image-container">
-    </div>
+
 </template>
-<style>
-.image-container {
-    width: 100%;
-    height: 100%;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-image: url(https://www.donnamoderna.com/content/uploads/2020/04/Viaggiare-organizzando-un-viaggio-830x625.jpg);
-}
-</style>
+<style></style>
