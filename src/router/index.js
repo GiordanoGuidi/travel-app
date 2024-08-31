651f184a77fb0821f212c30d817059b9d96cd0f8
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-08-31 14:32:40 +0200

    added route for the tripslist

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import TripDetails from '../pages/TripDetails.vue';
 import CreateTrip from '../pages/CreateTrip.vue';
 import HomePage from '../pages/HomePage.vue';
 import StopDetails from '../pages/StopDetails.vue';
+import TripList from '../pages/TripList.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -20,6 +21,8 @@ const router = createRouter({
         { path: '/trip/:id', name: 'trip-details', component: TripDetails },
         //Rotta per il dettaglio della tappa
         { path: '/stop/:id/:stop_id/:stop_day', name: 'stop-details', component: StopDetails },
+        //Rotta per la pagina con la lista dei viaggi
+        { path: '/trips', name: 'trip-list', component: TripList },
     ]
 });
 export { router };
