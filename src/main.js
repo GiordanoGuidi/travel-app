fd1a5b6ecd682b09d0dbec70fe7a18c5a41cb20b
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-09-01 16:40:15 +0200

    added Fontawesome library

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import { createApp } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { router } from './router'
 import App from './App.vue'
+// Importiamo la libreria generica di FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+// Importiamo il componente di FontAwesome
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// Importiamo le icone da utilizzare
+import {
+    faArrowLeft, faMagnifyingGlass,
+    faArrowDown, faChevronRight, faPlus,
+    faMinus, faTrashCan
+} from '@fortawesome/free-solid-svg-icons'
+// Icone da caricare
+library.add(faArrowLeft, faMagnifyingGlass, faArrowDown,
+    faChevronRight, faPlus, faMinus, faTrashCan);
+import { router } from './router'
 
-
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+// Rendiamo le icone disponibili globalmente
+app.component('FontAwesomeIcon', FontAwesomeIcon);
+app.use(router);
+app.mount('#app');
