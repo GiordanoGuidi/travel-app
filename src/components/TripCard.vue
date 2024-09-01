6ddc16ae92d0975cbe18aef8840004e048985ce7
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-09-01 16:40:55 +0200

    added triggerRemoveStop and updateStatus methods

--- src/components/TripCard.vue
+++ src/components/TripCard.vue
@@ -15,7 +15,11 @@ export default {
         addStop(dayIndex) {
             this.formActive = true;
             this.currentDayIndex = dayIndex;
-            this.newStop = { title: '', description: '', image: '../../../public/placeholder.jpg', address: '', status: 'undone' };
+            this.newStop = { title: '', description: '', image: '../../../public/placeholder.jpg', address: '', status: false };
+        },
+        //Metodo che avvisa della rimozione di una tappa
+        triggerRemoveStop(tripId, dayIndex, stop) {
+            this.$emit('trigger-remove-stop', tripId, stop.id, dayIndex)
         },
         //Metodo per chiudere il form di aggiunta della tappa
         closeForm() {
@@ -30,6 +34,9 @@ export default {
                 this.closeForm();
             }, 100);
         },
+        updateStatus(tripId, dayIndex, stop) {
+            this.$emit('trigger-update-stop-status', tripId, dayIndex, stop)
+        }
     }
 }
 </script>
@@ -45,20 +52,36 @@ export default {
             <!-- Giorno del viaggio -->
             <h3 class="mt-3">Giorno : {{ dayIndex + 1 }}</h3>
             <!-- Recupero l'array di tappe della giornata -->
-            <div v-for="(stop, stopIndex) in day.stops" :key="stopIndex">
-                <ul class="list-unstyled">
-                    <RouterLink v-if="this.trip" class="text-decoration-none text-dark"
+            <ul class="list-unstyled h-100 d-flex flex-column gap-4">
+                <div v-for="(stop, stopIndex) in day.stops" :key="stopIndex" class="d-flex gap-5">
+                    <!-- Al click sulla card eseguo il redirect alla pagina di dettaglio della tappa -->
+                    <RouterLink v-if="this.trip"
+                        class="text-decoration-none text-dark d-flex align-items-center gap-5 col-8"
                         :to="{ name: 'stop-details', params: { id: trip.id, stop_id: stop.id, stop_day: dayIndex } }">
-                        <li class="stop-card d-flex justify-content-between align-items-center mb-5 w-50">
-                            <p>{{ stop.title }}</p>
-                            <div class="img-container d-flex align-items-center">
-                                <img :src="stop.image" alt="">
+                        <li class="stop-card d-flex flex-column justify-content-between align-items-center w-100">
+                            <div class="d-flex justify-content-between align-items-center w-100">
+                                <!-- Titolo della tappa -->
+                                <p>{{ stop.title }}</p>
+                                <!-- Immagine della tappa -->
+                                <div class="img-container d-flex align-items-center">
+                                    <img :src="stop.image" alt="">
+                                </div>
                             </div>
-                            <a href="">ciaoo</a>
                         </li>
                     </RouterLink>
-                </ul>
-            </div>
+                    <div class="d-flex justify-content-center align-items-center gap-5">
+                        <!-- Bottone per eliminare la tappa -->
+                        <button class="delete-button" @click="triggerRemoveStop(trip.id, dayIndex, stop)" href="#">
+                            <font-awesome-icon class="text-danger pulse-icon" :icon="['fas', 'fa-trash-can']" />
+                        </button>
+                        <!-- Switcher per segnare la tappa come completata -->
+                        <div class="form-check form-switch">
+                            <input v-model="stop.status" @change="updateStatus(trip.id, dayIndex, stop)"
+                                class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                        </div>
+                    </div>
+                </div>
+            </ul>
             <div v-if="formActive && currentDayIndex === dayIndex">
                 <!-- Form -->
                 <AddStopForm :stop="newStop" :day="dayIndex" @close-form="closeForm" @submit-form="triggerSubmitForm" />
@@ -100,5 +123,34 @@ export default {
             max-height: 100%;
         }
     }
+
+    .delete-button {
+        background: none;
+        border: none;
+        padding: 0;
+        cursor: pointer;
+        font-size: 1.3rem;
+    }
+
+    .pulse-icon:hover {
+        animation: pulse 1.2s infinite;
+    }
+
+    /* Definisce l'animazione di pulsazione */
+    @keyframes pulse {
+        0% {
+            transform: scale(1);
+        }
+
+        50% {
+            transform: scale(1.8);
+            /* Aumenta le dimensioni del 20% */
+        }
+
+        100% {
+            transform: scale(1);
+        }
+    }
+
 }
 </style>
