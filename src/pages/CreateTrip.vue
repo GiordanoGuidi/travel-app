6180a6c267f835f350ec594cae17c4c454ad49a8
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-09-02 01:31:13 +0200

    modified endpoint

--- src/pages/CreateTrip.vue
+++ src/pages/CreateTrip.vue
@@ -1,7 +1,7 @@
 <script>
 import CreateTripForm from '../components/forms/CreateTripForm.vue';
 import { store } from '../data/store';
-const endpoint = 'https://localhost:8888/boolean/travel-app-back';
+const endpoint = 'https://prao-app.42web.io/travel-app-back';
 export default {
     name: 'CreateTrip',
     data: () => ({
